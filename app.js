2aa1704997b7278a4a136ef425ba43d182b69584
bella-mancini <89321746+bella-mancini@users.noreply.github.com>
2025-01-21 11:54:02 -0700

    js

--- app.js
+++ app.js
@@ -1,43 +1,25 @@
-const sheetName = 'rewoven-data'
-const scriptProp = PropertiesService.getScriptProperties()
-
-function intialSetup () {
-  const activeSpreadsheet = SpreadsheetApp.getActiveSpreadsheet()
-  scriptProp.setProperty('key', activeSpreadsheet.getId())
-}
-
-function doPost (e) {
-  const lock = LockService.getScriptLock()
-  lock.tryLock(10000)
-
-  try {
-    const doc = SpreadsheetApp.openById(scriptProp.getProperty('key'))
-    const sheet = doc.getSheetByName(sheetName)
-
-    const headers = sheet.getRange(1, 1, 1, sheet.getLastColumn()).getValues()[0]
-    const nextRow = sheet.getLastRow() + 1
-
-    const newRow = headers.map(function(header) {
-      return header === 'Date' ? new Date() : e.parameter[header]
-    })
-
-    sheet.getRange(nextRow, 1, 1, newRow.length).setValues([newRow])
-
-    return ContentService
-      .createTextOutput(JSON.stringify({ 'result': 'success', 'row': nextRow }))
-      .setMimeType(ContentService.MimeType.JSON)
-  }
-
-  catch (e) {
-    return ContentService
-      .createTextOutput(JSON.stringify({ 'result': 'error', 'error': e }))
-      .setMimeType(ContentService.MimeType.JSON)
-  }
-
-  finally {
-    lock.releaseLock()
-  }
-}
-
 // web url https://script.google.com/macros/s/AKfycbwNPHFaXw0cpHXC9Rus6y7UJvI8pyWIHjYa9vOW_1c3CRI7LhfvNgE4oHXb-iCWIqng/exec
 // deploy id AKfycbwNPHFaXw0cpHXC9Rus6y7UJvI8pyWIHjYa9vOW_1c3CRI7LhfvNgE4oHXb-iCWIqng
+
+document.getElementById('my-form').addEventListener('submit', function(e) {
+    e.preventDefault(); // Prevent default form submission
+    const form = e.target;
+
+    // Send the form data using Fetch API
+    fetch(form.action, {
+        method: form.method,
+        body: new FormData(form),
+    })
+    .then(response => {
+        if (response.ok) {
+            // Redirect to success.html
+            window.location.href = "success.html";
+        } else {
+            alert("There was an error submitting the form.");
+        }
+    })
+    .catch(error => {
+        console.error("Error:", error);
+        alert("Failed to submit the form.");
+    });
+});
