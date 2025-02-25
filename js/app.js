fc55dd669cbcf93ff5faf57c0c94e754623b4f44
bella-mancini <89321746+bella-mancini@users.noreply.github.com>
2025-02-25 12:26:22 -0700

    home page

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // web url https://script.google.com/macros/s/AKfycbwNPHFaXw0cpHXC9Rus6y7UJvI8pyWIHjYa9vOW_1c3CRI7LhfvNgE4oHXb-iCWIqng/exec
 // deploy id AKfycbwNPHFaXw0cpHXC9Rus6y7UJvI8pyWIHjYa9vOW_1c3CRI7LhfvNgE4oHXb-iCWIqng
 
-document.getElementById('my-form').addEventListener('submit', function(e) {
+/*document.getElementById('my-form').addEventListener('submit', function(e) {
     e.preventDefault(); // Prevent default form submission
     const form = e.target;
 
@@ -22,4 +22,29 @@ document.getElementById('my-form').addEventListener('submit', function(e) {
         console.error("Error:", error);
         alert("Failed to submit the form.");
     });
-});
+});*/
+
+/* Visibilty Nav bar */
+
+/*document.querySelector('.menu-icon').addEventListener('click', function () {
+    const contentNav = document.querySelector('.content-nav');
+    contentNav.classList.toggle('show');
+  });*/
+
+
+
+
+var coll = document.getElementsByClassName("menu-icon");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = document.querySelector('.content');
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
